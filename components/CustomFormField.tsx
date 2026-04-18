024cea96e9ff6831799cf611b5cb2e646b1ae726
Noushad Zaman <noushadozi333@gmail.com>
2026-04-18 22:45:27 +0600

    homepage client

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { Control, Controller } from "react-hook-form";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { FormFieldType } from "./forms/PatientForm";
+import Image from "next/image";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import { E164Number } from "libphonenumber-js/core";
+
+interface CustomProps {
+  control: any;
+  name: string;
+  fieldType: FormFieldType;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkelton?: (field: any) => React.ReactNode;
+}
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, iconAlt, iconSrc, placeholder } = props;
+
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || "icon"}
+              className="ml-2"
+            />
+          )}
+          <Input
+            placeholder={placeholder}
+            {...field}
+            className="shad-input border-0"
+          />
+        </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <div>
+          <PhoneInput
+            defaultCountry="US"
+            placeholder={placeholder}
+            international
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+          />
+        </div>
+      );
+
+    default:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+
+  return (
+    <div>
+      <FieldGroup>
+        <Controller
+          name={name}
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              {fieldType !== FormFieldType.CHECKBOX && label && (
+                <FieldLabel htmlFor="form-rhf-input-username">
+                  {label}
+                </FieldLabel>
+              )}
+
+              <RenderField field={field} props={props} />
+              {/* <Input
+                {...field}
+                id="form-rhf-input-username"
+                aria-invalid={fieldState.invalid}
+                placeholder="shadcn"
+                autoComplete="username"
+              /> */}
+              {/* <FieldDescription>
+                This is your public display name. Must be between 3 and 10
+                characters. Must only contain letters, numbers, and underscores.
+              </FieldDescription>
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />} */}
+            </Field>
+          )}
+        />
+      </FieldGroup>
+    </div>
+  );
+};
+
+export default CustomFormField;
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/router";
+
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTARE = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELTON = "skelton",
+}
+
+export function PatientForm() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      // const userData = { name, email, phone };
+      // const user = await createUser(userData);
+      // if (user) {
+      //   router.push(`/patients/${user.$id}/register`);
+      // }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="w-full sm:max-w-md">
+      <form id="form-rhf-input" onSubmit={form.handleSubmit(onSubmit)}>
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+      </form>
+
+      <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="name"
+        label="Full name"
+        placeholder="John Doe"
+        iconSrc="/assets/icons/user.svg"
+        iconAlt="user"
+      />
+
+      <CustomFormField
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="email"
+        label="Email"
+        placeholder="johndoe@gmail.com"
+        iconSrc="/assets/icons/email.svg"
+        iconAlt="email"
+      />
+
+      <CustomFormField
+        fieldType={FormFieldType.PHONE_INPUT}
+        control={form.control}
+        name="phone"
+        label="Phone number"
+        placeholder="(+880 1909827855)"
+      />
+      <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+    </div>
+  );
+}
+
+export default PatientForm;
