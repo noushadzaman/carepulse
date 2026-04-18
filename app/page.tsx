ee83234a8986e47efc0d8ade598f946399ba09b5
Noushad Zaman <noushadozi333@gmail.com>
2026-04-18 12:39:06 +0600

    setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-124"></div>
+      </section>
+    </div>
+  );
+}
